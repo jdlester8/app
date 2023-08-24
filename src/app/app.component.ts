4857e8d77cb0e94b00608cca85d216a899fbd53d
JD Lester <jd8lester@gmail.com>
2023-08-24 00:02:46 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  @ViewChild("#wrapper") wrapper!: ElementRef;
+  ngOnInit() {
+    d3.select(this.wrapper.nativeElement).append("svg").append("g");
+  }
 }
